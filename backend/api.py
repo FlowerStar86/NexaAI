758d63b440d5d4ad36c6b2dec49b98528dacaffe
FlowerStar86 <22501000239@lu.ac.ae>
2026-08-01 04:28:05 +0400

    Disable Flask debug mode

--- backend/api.py
+++ backend/api.py
@@ -366,4 +366,4 @@ def career_endpoint():
 
 if __name__ == '__main__':
     print("Starting Nexa AI Python Flask API Server on http://127.0.0.1:5000...")
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
